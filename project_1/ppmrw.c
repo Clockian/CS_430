67b110018474a5873b9c324ea895e59ffc67226f
Clockian <jrsaydyk@gmail.com>
2017-09-12 18:47:03 -0700

    Implemented initial file error checking

--- project_1/ppmrw.c
+++ project_1/ppmrw.c
@@ -7,21 +7,46 @@ int main(int argc, char *argv[]) {
 	if (argc != 4) {
 		printf("Usage: ppmrw CONVERT-TO-# INPUT-FILE OUTPUT-FILE\n");
 		return 1;
+	} else {
+		printf("Section 1: Success\n");
 	}
 
 	// Check if arg 1 is a 3 or 6
+	int magic_num;
+
+	magic_num = atoi(argv[1]);
+
+	if(magic_num != 3 && magic_num != 6) {
+		printf("Usage: Choose between '3' for P3 or '6' for P6\n");
+		return 1;
+	} else {
+		printf("Section 2: Success\n");
+	}
 	
 
 	// Check file names to see if they
-	// 	-End in .ppm
-	// 	-input and output filename are different
-	// 	-No negative "-" at the beginning
-	
+	// 	-Check if files exists
+	// 	-End in .ppm ### Scratch, not possible, magic number replicates this
+	// 	-input and output filename are different ### unable to do 
+	// 	without comparing everything in each other
+	// 	-No negative "-" at the beginning  ### Scratch, if it works, who cares
+	FILE *fr = fopen(argv[2], "r");
+	if(fr == NULL) {
+		printf("Input file does not exist\n");
+		return 1;
+	} else {
+		printf("Section 3.1: Success\n");
+	}
 
-	// Read input file into memory, else it can't be read from
-	
 
 	// Read "magic number" in the file for P3 or P6
+	char magicnum1 = fgetc(fr);
+	char magicnum2 = fgetc(fr);
+	char newline = fgetc(fr);
+	printf("%c%c%c", magicnum1, magicnum2, newline);
+
+	
+	// Read input file into memory, else it can't be read from
 	
 
 	// Skip any comments "#" to get the "HEIGHT WIDTH"
@@ -34,3 +59,6 @@ int main(int argc, char *argv[]) {
 	
 
 	// If converting from P6 to P3, convert the binary to ASCII decimal
+	
+
+}
